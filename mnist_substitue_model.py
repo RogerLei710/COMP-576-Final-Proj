fix normalization bug

mnist_substitue_model.py
@@ -75,7 +75,7 @@ def train_sub(model, x_train, y_train, x_test, y_test, epochs, lamda, aug_func='
 		if aug_func == 'jacobian':        
 			x_delta = lamda * tf.sign(x_gradient)
 		elif aug_func == '576final':        
-			normalized, _ = tf.linalg.normalize(x_gradient, ord='euclidean', axis=0)
+			normalized = tf.math.l2_normalize(x_gradient, axis=[1,2], epsilon=1e-12)
 			x_delta = lamda * normalized
 		else:
 			raise ValueError('Augmentation func {} not recognized'.format(aug_func))

util.py
@@ -12,7 +12,9 @@ def split_train_test(X, y, test_ratio=0.2):
 
     # Summarize each class' distribution
     unique, counts = np.unique(y_test, return_counts=True)
-    print("Summarize each class' distribution", dict(zip(unique, counts)))
+    print("(Test) Summarize each class' distribution", dict(zip(unique, counts)))
+    unique, counts = np.unique(y_train, return_counts=True)
+    print("(Train) Summarize each class' distribution", dict(zip(unique, counts)))
     return X_train, X_test, y_train, y_test
 
 def test_split_train_test():